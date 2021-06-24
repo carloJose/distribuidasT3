d30373fc145fe6c0eb2108bb9a4db0761810f55c
carloJose <carlo.jose1997@gmail.com>
2021-06-24 03:31:53 -0300

    so tirando arquivos nao necessarios

--- main.py
+++ main.py
@@ -20,19 +20,5 @@ def main(id):
     nodo.start()
     nodo.join()
 
-    # MCAST_GRP = '224.1.1.1'
-    # MCAST_PORT = 5007
-    # IS_ALL_GROUPS = True
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    # snd = threading.Thread(target=pst.send, args=(sock,))
-    # snd.start()
-    # recv = threading.Thread(target=pst.receieve, args=(sock,))
-    # recv.start()
-
-    # snd.join()
-    # recv.join()
-
 if __name__ == "__main__":
     main(int(sys.argv[1]))
