28789213d874325ff5517f53fa0b1933ff570a1c
carloJose <carlo.jose1997@gmail.com>
2021-07-03 03:59:46 -0300

    group

--- postman.py
+++ postman.py
@@ -80,7 +80,7 @@ class Postman(Thread):
         
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((MCAST_GRP, MCAST_PORT))
+        sock.bind(('', MCAST_PORT))
         mreq = struct.pack('4sl', socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         self.sockTop = sock
